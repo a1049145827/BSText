0afb9b8b309e70c28cfdcaa4ecc0b643b5db2136
Hongbo Liu <429699629@qq.com>
2019-12-12 16:54:34 +0800

    fix crash

--- BSText/String/TextRunDelegate.swift
+++ BSText/String/TextRunDelegate.swift
@@ -21,8 +21,11 @@ public class TextRunDelegate: NSObject, NSCopying, NSCoding, NSSecureCoding {
         get {
             // MARK: - 此处要使用本类对象，否则之后需要取出 Delegate 的 ConRef 的时候会出问题
             let extentBuffer = UnsafeMutablePointer<TextRunDelegate>.allocate(capacity: 1)
-            extentBuffer.pointee = self.copy() as! TextRunDelegate
-//            extentBuffer.pointee = self
+            if self.descent > 0 {
+                extentBuffer.pointee = self
+            } else {
+                extentBuffer.pointee = self.copy() as! TextRunDelegate
+            }
             
             var callbacks = CTRunDelegateCallbacks(version: kCTRunDelegateCurrentVersion, dealloc: { (pointer) in
                 
