64086d6cb08db7576282acb657c957202194f0ed
Developer <developer@example.com>
2026-05-13 14:38:31 +0800

    修复黑屏问题：SceneDelegate 正确设置窗口

--- DemoApp/SceneDelegate.swift
+++ DemoApp/SceneDelegate.swift
@@ -2,4 +2,12 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
 }
